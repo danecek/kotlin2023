d0f4dbd7e19d0008099691d7f2b5189e521b879e
Jiri Danecek <danecek@fit.cvut.cz>
2023-05-11 13:59:38 +0200

    u

--- Expr/src/main/kotlin/gopas/kot/expr/expr.kt
+++ Expr/src/main/kotlin/gopas/kot/expr/expr.kt
@@ -3,6 +3,9 @@ package gopas.kot.expr
 interface Expr {
     val value: Int
     val infix: String
+    companion object {
+        val One = Num(1)
+    }
 }
 
 /*class Num3(_value: Int) : Expr {
@@ -15,8 +18,15 @@ data class Num(override val value: Int) : Expr {
     override val infix = value.toString()
 }
 
+data class Var(val name: String) : Expr {
+    override val value: Int
+        get() = throw RuntimeException()
+    override val infix: String = name
+
+}
+
 enum class Oper(val txt: String){
-    PLS("+"), MLT("*");
+    PLS("+"), MNS("-"), MLT("*");
     override fun toString(): String = txt
 }
 
@@ -25,9 +35,21 @@ data class BinOp(val op: Oper, val left: Expr, val right: Expr): Expr {
     override val value: Int
         get() = when (op) {
             Oper.PLS -> left.value + right.value
+            Oper.MNS -> left.value + right.value
             Oper.MLT -> left.value * right.value
         }
 }
+
+data class UnOp(val op: Oper, val oprnd: Expr): Expr {
+    override val infix: String = "$$op ${oprnd.infix}"
+    override val value: Int
+        get() = when (op) {
+            Oper.PLS -> oprnd.value
+            Oper.MNS -> - oprnd.value
+            else -> error("fail")
+        }
+}
+
 
 
 fun main() {
